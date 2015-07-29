4059b539d3e41887445bba55e2fccc8020d53c01
Jack Yang <jy7.yang@gmail.com>
2015-07-29 18:39:23 -0400

    remove passport login on server

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var path = require('path');
 
-var passport = require('passport')
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
 var app = express();
 var hbs = exphbs.create({
   defaultLayout: 'main',
@@ -13,39 +10,11 @@ var hbs = exphbs.create({
 });
 
 var userObj = null;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new GoogleStrategy({
-    clientID: '639034003280-d800g8nadmhjr1h9n9gva90h2iop8ah4.apps.googleusercontent.com',
-    clientSecret: 'A80N_Cb4W0WfAnZ-6fnZmY9V',
-    callbackURL: 'http://localhost:8080/auth/google/callback'
-  },
-  function(accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  userObj = user;
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-app.get('/login',
-  passport.authenticate('google', { scope: 'https://www.googleapis.com/auth/plus.login' })
-);
-
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  }
-);
+module.exports = function(user) {
+  // Allow upper level web app to pass user information.
+  userObj.name = user.name;
+  userObj.id = user.id;
+}
 
 app.use('/css', express.static('css'));
 app.use('/fonts', express.static('fonts'));
@@ -61,13 +30,7 @@ app.locals.layout = false;
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function(req, res) {
-  res.render('main', {title:'Home', user:userObj});
-});
-
-app.get('/logout', function(req, res) {
-  userObj = null;
-  res.redirect('/');
-  // res.render('main', {title:'Home', user:userObj});
+  res.render('main', {title:'Home'});
 });
 
 app.get('/templateEditor', function(req, res) {
