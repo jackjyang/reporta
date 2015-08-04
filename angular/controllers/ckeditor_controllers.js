66de2db5969fac68c388c57e910235b13f1104ef
Jerry Cheng <jerrychengcz@hotmail.com>
2015-08-04 09:47:55 -0400

    add readonly config and changed recipes ckeditor

--- angular/controllers/ckeditor_controllers.js
+++ angular/controllers/ckeditor_controllers.js
@@ -1,10 +1,8 @@
 var reportaApp = angular.module('reporta');
 
 reportaApp.controller('ckeditorRecipeController', function($scope) {
-  CKEDITOR.config.disableAutoInline = true;
-  CKEDITOR.config.readOnly = true;
-
-  // TODO: need to actually remove button
-
-  CKEDITOR.replace('editor1');
+  CKEDITOR.replace('editor1',
+    {
+      customConfig : '/ckeditor/config_readonly.js'
+    });
 });
---
+++ ckeditor/config_readonly.js
@@ -0,0 +1,32 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+
+  // Define changes to default configuration here. For example:
+  // config.language = 'fr';
+  // config.uiColor = '#AADC6E';
+
+  config.extraPlugins = 'findelement';
+  config.disableAutoInline = true;
+  // Define changes to default configuration here.
+  // For complete reference see:
+  // http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+  // The toolbar groups arrangement, optimized for two toolbar rows.
+
+  config.toolbarGroups = [
+    { name: 'insert' }
+  ];
+
+  config.removeButtons = 'Image,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe';
+
+  // Simplify the dialog windows.
+  config.removeDialogTabs = 'image:advanced;link:advanced';
+
+  config.readOnly = true;
+  config.resize_enabled = false;
+
+};
