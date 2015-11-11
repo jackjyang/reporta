b9b8ede5c3f8959cc91fb54c01be8e4145821fc4
Jack Yang <jy7.yang@gmail.com>
2015-11-10 18:53:54 -0800

    fix forms api

--- acerta_api/get_report_form.py
+++ acerta_api/get_report_form.py
@@ -14,17 +14,18 @@ r = s.post(urlLogin, params=AUTH)
 # Input args and help
 
 def printHelp():
-  print "Default Usage: python get_report_form.py analysis_name system_name trace_name"
+  print "Default Usage: python get_report_form.py analysis_type system_name comma_separated_trace_names"
 
 if len(SYSTEM.argv) > 1 and SYSTEM.argv[1] == "-h":
   printHelp()
   SYSTEM.exit(0)
 
-if len(SYSTEM.argv) < 2:
+if len(SYSTEM.argv) < 4:
   printHelp()
   SYSTEM.exit(1)
 
-sysPayload = { 'analysis': SYSTEM.argv[1], 'system': SYSTEM.argv[2], 'trace': SYSTEM.argv[3] }
+traces = SYSTEM.argv[3].split(',')
+sysPayload = { 'analysis': SYSTEM.argv[1], 'system': SYSTEM.argv[2], 'traces': traces }
 
 r = s.post(urlForms, data=json.dumps(sysPayload))
 
